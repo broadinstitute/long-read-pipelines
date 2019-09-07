9d392f1973d65e950f7f8294eb8cd303249010cf
Steve Huang <shuang@broadinstitute.org>
2019-09-06 20:00:40 -0400

    move validate.wdls.sh to scripts

--- scripts/validate.wdls.sh
+++ scripts/validate.wdls.sh
@@ -2,6 +2,10 @@
 
 # we do not set eu here because the WDLs may break and we want to catch that then continue
 
+if [[ "$#" -eq 1 ]]; then
+  cd $1
+fi
+
 echo "VALIDATING WDL FILES IN THE CURRENT DIRECTORY"
 (womtool --version || echo "I need womtool to run" ) && echo
 
