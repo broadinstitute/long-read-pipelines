839ca06cc88c0c62cdce1ad1a0c9b649ea7b2014
Kiran Garimella <kiran@broadinstitute.org>
2019-10-02 19:50:52 -0400

    Fixed a bad awk substitution that was causing a corrupt conversion from fastq to BAM format

--- docker/lr-align/prepare_run.py
+++ docker/lr-align/prepare_run.py
@@ -19,11 +19,11 @@ for arg in sys.argv[1:]:
 if len(fqs) > 0:
     # Fastq files don't have a read group ID to auto-detect, so make one up from a sha1 code of the input files.
     hc = hashlib.sha1(",".join(fqs).encode()).hexdigest()[0:8]
-    header = "\t".join(['@HD', 'VN:1.5', 'SO:unknown'])
-    rg = "\t".join(['@RG', 'ID:' + hc, 'SM:unknown'])
+    header = "\\t".join(['@HD', 'VN:1.5', 'SO:unknown'])
+    rg = "\\t".join(['@RG', 'ID:' + hc, 'SM:unknown'])
 
     # This rather complicated one-liner manually transforms a collection of fastq files into an unmapped BAM file.
-    cmd = f'((echo "{header}") && (echo "{rg}") && (zcat {" ".join(fqs)} | paste - - - - | sed "s/^@//" | awk -F "\\t" \'{{ a = sub(/ .+/, "", $1); print $a, "4", "*", "0", "255", "*", "*", "0", "0", $2, $4, "RG:Z:{hc}" }}\' | sed "s/ /\\t/g")) | samtools view -b > unmapped.bam'
+    cmd = f'((echo "{header}") && (echo "{rg}") && (zcat < {" ".join(fqs)} | paste - - - - | sed "s/^@//" | awk -F "\\t" \'{{ sub(/ .+/, "", $1); print $1, "4", "*", "0", "255", "*", "*", "0", "0", $2, $4, "RG:Z:{hc}" }}\' | sed "s/ /\\t/g")) | samtools view -b > unmapped.bam'
 elif len(bams) > 1:
     # Merge multiple unmapped BAM files into a single unmapped BAM file.
     cmd = f'samtools cat -o unmapped.bam {" ".join(bams)}'
