27a05d75b5ad7843d0089f9861b5cd065856b84c
Kiran V Garimella <kiran@broadinstitute.org>
2020-04-01 13:31:20 -0400

    Give more information about the files that match and don't match expectations

--- scripts/ci/run_test_suite.py
+++ scripts/ci/run_test_suite.py
@@ -163,7 +163,9 @@ def find_outputs(input_json):
 def compare_outputs(test, outs):
     ret = 0
     for b in outs:
-        if outs[b]['exp'] != outs[b]['act']:
+        if outs[b]['exp'] == outs[b]['act']:
+            print_failure(f"{test}: {b}: {outs[b]['exp_path']} ({outs[b]['exp']}) == {outs[b]['act_path']} ({outs[b]['act']})")
+        else:
             print_failure(f"{test}: {b}: {outs[b]['exp_path']} ({outs[b]['exp']}) != {outs[b]['act_path']} ({outs[b]['act']})")
             ret = 1
 
