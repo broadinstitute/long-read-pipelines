ca527d1017e4bbb6d7c2fddc36ad0c3423fe0e26
Kiran V Garimella <kiran@broadinstitute.org>
2020-05-31 18:39:48 -0400

    Fixed typo

--- scripts/ci/run_test_suite.py
+++ scripts/ci/run_test_suite.py
@@ -226,7 +226,7 @@ def compare_outputs(test, outs):
 
     num_mismatch = 0
     for b in outs:
-        if not b.endswith(".png") and not b.endswith(".sequencing_summary.txt") and outs[b]['exp'] != outs[b]['act']:
+        if not b.endswith(".png") and not b.endswith("sequencing_summary.txt") and outs[b]['exp'] != outs[b]['act']:
             if outs[b]['act_path'] is None or compare_contents(outs[b]['exp_path'], outs[b]['act_path']) != 0:
                 print_info(f'- {b} versions are different:')
                 print_failure(f"    exp: ({outs[b]['exp']}) {outs[b]['exp_path']}")
