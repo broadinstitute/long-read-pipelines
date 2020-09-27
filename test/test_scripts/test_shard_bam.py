1476feea0f63036bf6080f582aa2d5131dd638e4
Kiran V Garimella <kiran@broadinstitute.org>
2020-09-27 16:14:56 -0400

    Much better test. Don't allow __pycache__ and test_output directory from being commited to the repo.

--- test/test_scripts/test_shard_bam.py
+++ test/test_scripts/test_shard_bam.py
@@ -1,12 +1,47 @@
-def test_shard_bam(script_runner):
-    ret = script_runner.run("../../docker/lr-pb/shard_bam.py", "-p", "shard", "-n", "2", "test/data/tiny.bam")
+import pysam
+import os
 
-    exp = """Reading index (test/data/tiny.bam.pbi). This may take a few minutes...
-Writing 2 shards using 2 threads...
-  - wrote 18 reads to shard0.bam
-  - wrote 27 reads to shard1.bam
-Sharded 45/45 reads across 2 shards.
-"""
+
+def get_read_zmw_counts(file):
+    bf = pysam.Samfile(file, 'rb', check_sq=False)
+
+    zmw_counts = {}
+    for read in bf:
+        zmw = read.get_tag("zm")
+        zmw_counts[zmw] = zmw_counts.get(zmw, 0) + 1
+
+    bf.close()
+
+    return zmw_counts
+
+
+def test_shard_bam(script_runner):
+    bam = "test/data/tiny.bam"
+    testdir = "test/test_output"
+    prefix = f"{testdir}/shard"
+    num_shards = 2
+
+    os.mkdir(testdir)
+    ret = script_runner.run("../../docker/lr-pb/shard_bam.py", "-p", prefix, "-n", str(num_shards), bam)
 
     assert ret.success
-    assert ret.stdout == exp
+
+    files_with_zmws = {}
+    zmw_counts_orig = get_read_zmw_counts(bam)
+    for i in range(0, num_shards):
+        zmw_counts_shard = get_read_zmw_counts(f'{prefix}{i}.bam')
+
+        for zmw in zmw_counts_shard:
+            zmw_counts_orig[zmw] = zmw_counts_orig.get(zmw, 0) - zmw_counts_shard.get(zmw, 0)
+
+            if zmw not in files_with_zmws:
+                files_with_zmws[zmw] = set()
+
+            files_with_zmws[zmw].add(i)
+
+    for zmw in files_with_zmws:
+        # Verify that ZMWs only ever appear in one shard.
+        assert len(files_with_zmws[zmw]) == 1
+
+        # Verify that every instance of a ZMW seen in the original file are accounted for in the shards.
+        assert zmw_counts_orig[zmw] == 0
