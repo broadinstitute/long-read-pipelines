d03b360f685fb10a3ed0350ad513128077fb8ff1
Jonn Smith <jonn@broadinstitute.org>
2021-10-13 11:54:18 -0400

    Updated mas_seq_demux_by_index.py to include read counts.

--- docker/lr-transcript_utils/python/mas_seq_demux_by_index.py
+++ docker/lr-transcript_utils/python/mas_seq_demux_by_index.py
@@ -4,7 +4,12 @@ import pysam
 import time
 import math
 import sys
+import os
 import ssw
+import gzip
+
+from construct import *
+
 from tqdm import tqdm
 
 index_sequences = [
@@ -32,6 +37,31 @@ index_tag = "XI"
 
 ################################################################################
 
+def load_read_count(pbi_file):
+    """Compute file offsets for specified read names"""
+
+    # Decode PacBio .pbi file.  This is not a full decode of the index, only the parts we need
+    # until we get to the read count.
+    # More on index format at https://pacbiofileformats.readthedocs.io/en/9.0/PacBioBamIndex.html .
+
+    fmt = Struct(
+        # Header
+        "magic" / Const(b"PBI\x01"),
+        "version_patch" / Int8ul,
+        "version_minor" / Int8ul,
+        "version_major" / Int8ul,
+        "version_empty" / Int8ul,
+        "pbi_flags" / Int16ul,
+        "n_reads" / Int32ul,
+        )
+
+    with gzip.open(pbi_file, "rb") as f:
+        idx_contents = fmt.parse_stream(f)
+
+        return idx_contents.n_reads
+
+################################################################################
+
 t_start = time.time()
 
 base_folder = "."
@@ -47,6 +77,11 @@ with pysam.AlignmentFile(f"{input_bam}", "rb", check_sq=False, require_index=Fal
         desc="Demultiplexing indexed reads", unit="read") as pbar:
 
     out_base_file_name = input_bam[:input_bam.find(".bam")] + ".demux_"
+
+    pbi = f"{input_bam}.pbi"
+    read_count = None
+    if os.path.exists(pbi):
+        read_count = load_read_count(pbi)
 
     epsilon = 1 - (ambiguity_thresh_pct / 100)
 
