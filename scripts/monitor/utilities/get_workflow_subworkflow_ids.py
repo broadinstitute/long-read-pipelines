fd433c34502a30742dc60e13baf1dd4e4ee7ffbf
Steve Huang <shuang@broadinstitute.org>
2021-10-15 10:33:21 -0400

    fix a bug in utility script for extracting sub-workflow IDs from provided metadata json for a Cromwell run

--- scripts/monitor/utilities/get_workflow_subworkflow_ids.py
+++ scripts/monitor/utilities/get_workflow_subworkflow_ids.py
@@ -1,35 +1,60 @@
 #!/usr/bin/env python3
 
 import json
+import sys
+import os
 import pprint
 from datetime import datetime
 
+
 class GetWorkflowIdTree:
+
+    """
+    Given slim-metadata JSON file, parse the root- and sub-workflow ID tree.
+    """
 
     ROOT_ID = 'ROOT_ID'
 
     def __init__(self, slim_metadata_file):
-        self.subworkflows_and_their_levels = dict()
         with open(slim_metadata_file, 'r') as json_file:
             self.slim_metadata = json.load(json_file)
 
     @staticmethod
-    def _get_time_elapsed(dictionary, DATE_TIME_FMT = "%Y-%m-%dT%H:%M:%S.%f%z"):
-        start_time = datetime.strptime(dictionary['start'], DATE_TIME_FMT)
-        end_time = datetime.strptime(dictionary['end'], DATE_TIME_FMT)
-        tdelta = end_time - start_time
-        msg = f"Started on {start_time} and finished on {end_time} (totaling {tdelta})."
-        return(msg)
+    def _get_time_elapsed(task_metadata, dt_format="%Y-%m-%dT%H:%M:%S.%f%z"):
+        """
+        Mostly for debugging
+        @param task_metadata:
+        @param dt_format:
+        @return:
+        """
+        start_time = datetime.strptime(task_metadata['start'], dt_format)
+        end_time = datetime.strptime(task_metadata['end'], dt_format)
+        delta = end_time - start_time
+        msg = f"Started on {start_time} and finished on {end_time} (totaling {delta})."
+        return msg
 
     @staticmethod
     def _deal_with_subworkflow_or_task(this_level_metadata,
                                        parent_level,
                                        parent_level_wid,
-                                       parent_level_dict,
-                                       debug = False):
+                                       parent_name=None,
+                                       debug=False):
 
-        local_dict = dict()
+        """
+        Major workpiece, recursively parse (DFS) tree.
+        @param this_level_metadata:
+        @param parent_level:
+        @param parent_level_wid:
+        @param debug:
+        @return:
+        """
+
+        subworkflow_info_for_this_node = dict()
         current_level = 1 + parent_level
+
+        pretty_parent_name = parent_name
+        if 0 == parent_level and parent_name is None:
+            pretty_parent_name = list(this_level_metadata.keys())[0].split(".")[0]
 
         for unit_name, metadata in this_level_metadata.items():
 
@@ -37,60 +62,76 @@ class GetWorkflowIdTree:
             #   if the unit is a preemptible task, several attempts may be made,
             #     then the elements in 'metadata' will be all the attempts actually made for the task
             #   otherwise it's a subworkflow (which itself may contain its own subworkflows)
-            #     and we assume that subworkflows are not attempted multiple times (hence 1 == len)
+            #     and we assume that
+            #       1) simple tasks don't have their own sub-workflows
+            #       2) (sub-)workflows are not attempted multiple times, only tasks are
             for struct in metadata:
 
                 time_stamps = GetWorkflowIdTree._get_time_elapsed(struct)
-                this_is_simple_task = not 'subWorkflowMetadata' in list(struct.keys())
 
-                if ( this_is_simple_task ):
-                    if (debug):
-                        task_name = unit_name; # just a rename to better reflect what it is
-                        attempt = struct
-                        numb_attempt = attempt['attempt']
-                        backend_status = attempt['backendStatus']
-                        print(f"For simple task {task_name} on attempt {numb_attempt}, its backend status is {backend_status} (timewise: {time_stamps}).")
-                else:
-                    subworkflow_name = unit_name; # just a rename to better reflect what it is
+                is_subworkflow = 'subWorkflowMetadata' in struct
+
+                if is_subworkflow:
+                    # just a rename to better reflect what it is
+                    subworkflow_name = unit_name
                     subworkflow = struct
+
+                    pretty_name = pretty_parent_name + "." + subworkflow_name.split(".")[-1]
 
                     subworkflow_metadata = subworkflow['subWorkflowMetadata']
                     subworkflow_id = subworkflow_metadata['id']
 
-                    local_dict[ subworkflow_name ] = {"wid": subworkflow_id, \
-                                                      "level": current_level, \
-                                                      "parent": parent_level_wid}
+                    subworkflow_info_for_this_node[pretty_name] = {"wid": subworkflow_id,
+                                                                   "level": current_level,
+                                                                   "parent": parent_level_wid}
+                    # DFS
+                    child_subworkflow_info = GetWorkflowIdTree._deal_with_subworkflow_or_task(subworkflow_metadata['calls'],
+                                                                                              current_level, subworkflow_id,
+                                                                                              pretty_name, debug)
+                    if 0 < len(child_subworkflow_info):
+                        merged_dict = {**subworkflow_info_for_this_node, **child_subworkflow_info}
+                        subworkflow_info_for_this_node = merged_dict
+                    elif debug:
+                        print(f"For subworkflow {subworkflow_name}, its id is {subworkflow_id}, "
+                              f"  its status is {subworkflow['executionStatus']} "
+                              f"  (time-wise: {time_stamps}).")
+                elif debug:
+                    task_name = unit_name  # just a rename to better reflect what it is
+                    attempt = struct  # just a rename to better reflect what it is
+                    numb_attempt = attempt['attempt']
+                    backend_status = attempt['backendStatus']
+                    print(f"For simple task {task_name} on attempt {numb_attempt}, "
+                          f"  its backend status is {backend_status}"
+                          f"  (time-wise: {time_stamps}).")
 
-                    has_further_subworkflow = 'subWorkflowMetadata' in list(subworkflow_metadata['calls'].keys())
-                    if (has_further_subworkflow):
-                        child_dict = GetWorkflowIdTree.__deal_with_subworkflow_or_task(subworkflow_metadata['calls'], current_level, subworkflow_id, local_dict, debug)
-                        merged_dict = {**local_dict, **child_dict}
-                        local_dict = merged_dict
-                    elif (debug):
-                        print(f"For subworkflow {subworkflow_name}, its id is {subworkflow_id}, its status is {subworkflow['executionStatus']} (timewise: {time_stamps}).")
+        return subworkflow_info_for_this_node
 
-        return(local_dict)
+    def parse(self, debug=False):
+        """
+        Main interface
+        @param debug:
+        @return:
+        """
 
-    def parse(self, debug = False):
+        tree = dict()
+
         root_level_id = self.slim_metadata['id']
+        tree[GetWorkflowIdTree.ROOT_ID] = root_level_id
 
-        self.subworkflows_and_their_levels = GetWorkflowIdTree._deal_with_subworkflow_or_task(self.slim_metadata['calls'],
-                                                                                              0, root_level_id,
-                                                                                              dict(), debug)
+        calls = self.slim_metadata['calls']
+        child_tree = GetWorkflowIdTree._deal_with_subworkflow_or_task(calls, 0, root_level_id, debug=debug)
 
-        self.subworkflows_and_their_levels[GetWorkflowIdTree.ROOT_ID] = root_level_id
+        tree = {**tree, **child_tree}
 
         pp = pprint.PrettyPrinter(indent=2)
-        pp.pprint(self.subworkflows_and_their_levels)
+        pp.pprint(tree)
 
 
 ############################################################
 
 if __name__ == "__main__":
-    import sys
 
     if 1 == len(sys.argv):
-        import os
         my_name = os.path.basename(__file__)
         print(f"Usage: ./{my_name} <slim_metadata_json_file>")
     else:
