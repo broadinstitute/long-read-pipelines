85fc24f289ece061ac81cec57bcb419cc9619614
Jonn Smith <jonn@broadinstitute.org>
2022-03-22 12:46:53 -0400

    Fixed issue with RC reads.

--- docker/lr-10x/update_umi_positions.py
+++ docker/lr-10x/update_umi_positions.py
@@ -30,6 +30,52 @@ UMI_TAG = "ZU"
 NEW_UMI_TAG = "ZX"
 
 
+# IUPAC RC's from: http://arep.med.harvard.edu/labgc/adnan/projects/Utilities/revcomp.html
+# and https://www.dnabaser.com/articles/IUPAC%20ambiguity%20codes.html
+RC_BASE_MAP = {
+    "N": "N",
+    "A": "T",
+    "T": "A",
+    "G": "C",
+    "C": "G",
+    "Y": "R",
+    "R": "Y",
+    "S": "S",
+    "W": "W",
+    "K": "M",
+    "M": "K",
+    "B": "V",
+    "V": "B",
+    "D": "H",
+    "H": "D",
+    "n": "n",
+    "a": "t",
+    "t": "a",
+    "g": "c",
+    "c": "g",
+    "y": "r",
+    "r": "y",
+    "s": "s",
+    "w": "w",
+    "k": "m",
+    "m": "k",
+    "b": "v",
+    "v": "b",
+    "d": "h",
+    "h": "d",
+}
+
+
+def reverse_complement(base_string):
+    """
+    Reverse complements the given base_string.
+    :param base_string: String of bases to be reverse-complemented.
+    :return: The reverse complement of the given base string.
+    """
+
+    return "".join(map(lambda b: RC_BASE_MAP[b], base_string[::-1]))
+
+
 def ssw_build_matrix(match_score=2, mismatch_score=1):
     """
     Builds a matrix of ctypes values for matches and mismatches for the Smith-Waterman algorithm
@@ -76,12 +122,12 @@ def to_int(seq, lEle, dEle2Int):
     return num
 
 
-def get_alignment(ssw, sequence, adapter_sequence, alphabet, letter_to_int, mat, open_penalty=2, extension_penalty=1):
+def get_alignment(ssw, sequence, adapter_seq, alphabet, letter_to_int, mat, open_penalty=2, extension_penalty=1):
     """
     Performs the alignment of the read end to the adapter sequence
     :param ssw: ssw object for performing the Smith-Waterman alignment
     :param sequence: The sequence of the read end
-    :param adapter_sequence: The adapter sequence to align to
+    :param adapter_seq: The adapter sequence to align to
     :param alphabet: The alphabet object for the ssw algorithm
     :param letter_to_int: The dict to convert base letters to numbers
     :param mat: The match-mismatch score matrix for the ssw algorithm
@@ -90,15 +136,29 @@ def get_alignment(ssw, sequence, adapter_sequence, alphabet, letter_to_int, mat,
     :return: A tuple containing the position of the first and last base of the adapter sequence in the read end
     """
     sequence_numbers = to_int(sequence, alphabet, letter_to_int)
-    adapter_numbers = to_int(adapter_sequence, alphabet, letter_to_int)
+    adapter_numbers = to_int(adapter_seq, alphabet, letter_to_int)
 
     flag = 1
-    mask_length = len(adapter_sequence) // 2 if len(adapter_sequence) >= 30 else 15
+    mask_length = len(adapter_seq) // 2 if len(adapter_seq) >= 30 else 15
 
     q_profile = ssw.ssw_init(sequence_numbers, ctypes.c_int32(len(sequence)), mat, len(alphabet), 2)
 
-    res = ssw.ssw_align(q_profile, adapter_numbers, ctypes.c_int32(len(adapter_sequence)), open_penalty,
+    res = ssw.ssw_align(q_profile, adapter_numbers, ctypes.c_int32(len(adapter_seq)), open_penalty,
                         extension_penalty, flag, 0, 0, mask_length)
+
+    print()
+    print(sequence)
+    print(" " * res.contents.nQryBeg, end="")
+    print(adapter_seq)
+
+    print()
+    for f in (dir(res.contents)):
+        if not f.startswith("_"):
+            print(f"res.contents.{f} = ", end="")
+            print(eval(f"res.contents.{f}"))
+    print()
+
+
 
     return (res.contents.nQryBeg, res.contents.nQryEnd) if res.contents.nScore > 30 else None
 
@@ -129,24 +189,41 @@ def main(bam_filename, output_prefix, barcode_seq, cell_barcode_tag, umi_length,
 
                 num_reads += 1
 
-                # Let's just align the CBC and UMI
+                # Let's align the CBC and UMI in the forward direction:
                 alignments = get_alignment(ssw,
                                            read.query_sequence[:num_read_bases_to_align],
                                            barcode_seq + read.get_tag(cell_barcode_tag),
                                            alphabet,
                                            letter_to_int,
                                            mat)
+                if alignments:
 
-                if not alignments:
-                    print(f"Could not find adapter + CBC for read: {read.query_name}", file=sys.stderr)
-                    num_reads_without_new_umi += 1
-                    continue
+                    # OK, we have our alignment:
+                    alignment_end = alignments[1]
 
-                # OK, we have our alignment:
-                alignment_end = alignments[1]
+                    new_umi_seq = read.query_sequence[alignment_end + 1:alignment_end + 1 + umi_length]
+                    num_new_umis += 1
 
-                new_umi_seq = read.query_sequence[alignment_end+1:alignment_end + 1 + umi_length]
-                num_new_umis += 1
+                # OK, now let's try the RC direction:
+                else:
+                    rc_seq = reverse_complement(read.query_sequence)
+                    rc_alignments = get_alignment(ssw,
+                                               rc_seq[:num_read_bases_to_align],
+                                               barcode_seq + read.get_tag(cell_barcode_tag),
+                                               alphabet,
+                                               letter_to_int,
+                                               mat)
+
+                    if not rc_alignments:
+                        print(f"Could not find adapter + CBC for read: {read.query_name}", file=sys.stderr)
+                        num_reads_without_new_umi += 1
+                        continue
+
+                    # OK, we have our alignment:
+                    alignment_end = rc_alignments[1]
+
+                    new_umi_seq = rc_seq[alignment_end + 1:alignment_end + 1 + umi_length]
+                    num_new_umis += 1
 
                 if read.has_tag(existing_umi_tag) and read.get_tag(existing_umi_tag) == new_umi_seq:
                     num_new_umis_same_as_old += 1
