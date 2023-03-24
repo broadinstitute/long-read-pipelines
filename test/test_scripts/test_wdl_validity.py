0edbc31206f22546024cbad77c65e6aa90c86536
bshifaw <bshifaw@broadinstitute.com>
2023-03-24 18:08:40 -0400

    added test for miniwdl validation

--- test/test_scripts/test_wdl_validity.py
+++ test/test_scripts/test_wdl_validity.py
@@ -7,21 +7,25 @@ from pathlib import Path
 
 
 @pytest.mark.parametrize(
-    "dir_name",
-    ["pipelines", "tasks", "deprecated_wdl"],
+    "tool, subcommand, dir_names",
+    (
+        ["womtool", "validate", ["pipelines", "tasks", "deprecated_wdl"]],
+        ["miniwdl",  "check", ["pipelines", "tasks", "deprecated_wdl"]],
+    )
 )
-def test_wdl_validity(script_runner, dir_name):
-    retsum = 0
+def test_wdl_validity(script_runner, tool: str, subcommand: str, dir_names: list):
 
-    wdl_dir = Path(__file__).resolve().parents[2] / dir_name
-    if not wdl_dir.exists():
-        raise FileNotFoundError(
-            errno.ENOENT, os.strerror(errno.ENOENT), wdl_dir)
-    wdl_paths = wdl_dir / "**/*.wdl"
+    for dir_name in dir_names:
+        retsum = 0
+        wdl_dir = Path(__file__).resolve().parents[2] / dir_name
+        if not wdl_dir.exists():
+            raise FileNotFoundError(
+                errno.ENOENT, os.strerror(errno.ENOENT), wdl_dir)
+        wdl_paths = wdl_dir / "**/*.wdl"
 
-    for wdl in glob.glob(str(wdl_paths), recursive=True):
-        print(f'{wdl}:')
-        ret = subprocess.run(["womtool", "validate", wdl])
-        retsum += ret.returncode
+        for wdl in glob.glob(str(wdl_paths), recursive=True):
+            print(f'{wdl}:')
+            ret = subprocess.run([tool, subcommand, wdl])
+            retsum += ret.returncode
 
-    assert retsum == 0
+        assert retsum == 0
