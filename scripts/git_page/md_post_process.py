3a553fe5c041cdd33a435ea6a9561c73b50184f9
bshifaw <bshifaw@broadinstitute.com>
2023-04-12 11:16:11 -0400

    seperated read and write operation in funciton

--- scripts/git_page/md_post_process.py
+++ scripts/git_page/md_post_process.py
@@ -116,7 +116,7 @@ def remove_subheaders_after_first(md_path: Union[str, Path]) -> None:
     :return: None
     """
     try:
-        with Path(md_path).open('r+') as md_file:
+        with Path(md_path).open('r') as md_file:
             lines = md_file.readlines()
 
             second_subhead_index = None
@@ -127,11 +127,16 @@ def remove_subheaders_after_first(md_path: Union[str, Path]) -> None:
                     if count == 2:
                         second_subhead_index = i
                         break
-            if second_subhead_index is None:
-                return
-            md_file.seek(0)
-            md_file.truncate()
-            md_file.writelines(lines[:second_subhead_index])
+
+        if second_subhead_index is None:
+            return
+        else:
+            Logger.debug(f"Removing subheaders after index {second_subhead_index}")
+            with Path(md_path).open('w') as md_file:
+                md_file.seek(0)
+                md_file.truncate()
+                md_file.writelines(lines[:second_subhead_index])
+                md_file.write("\n") if lines[-1] != "\n" else None
 
     except (FileNotFoundError, IsADirectoryError):
         Logger.debug(f"Unable to open file at {md_path}")
