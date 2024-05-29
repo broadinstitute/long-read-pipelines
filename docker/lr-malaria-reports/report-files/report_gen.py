87475b79e6dcfe57190b03d93f9558f4e68933be
Bridget Knight <bknight@broadinstitute.org>
2024-05-29 19:07:10 -0400

    add defaults to some inputs

--- docker/lr-malaria-reports/report-files/report_gen.py
+++ docker/lr-malaria-reports/report-files/report_gen.py
@@ -630,7 +630,7 @@ if __name__ == '__main__':
     parser.add_argument("--num_reads_q15", help="the number of reads where the probability of a given base call being wrong is approximately 1 in 32", required=True)
 
     # Sequencing Summary
-    parser.add_argument("--sample_type", help="type of sample", default="N/A")
+    parser.add_argument("--sample_type", help="type of sample", default="N/A", nargs="?")
     parser.add_argument("--analysis_success", help="whether the analysis process completed successfully", required=True)
     parser.add_argument("--aligned_bases", help="total number of bases aligned to the reference genome", required=True)
     parser.add_argument("--aligned_reads", help="total number of reads aligned to the reference genome", required=True)
