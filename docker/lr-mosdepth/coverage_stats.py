ef3574f0b45b6ced6e5fa01ad7ba0d014460441f
bshifaw <bshifaw@broadinstitute.com>
2024-08-15 13:52:08 -0400

    "Addition: changed single to double quotes

--- docker/lr-mosdepth/coverage_stats.py
+++ docker/lr-mosdepth/coverage_stats.py
@@ -8,15 +8,15 @@ import pandas as pd
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Process coverage values.')
-    parser.add_argument('mosdepth_regions', type=str,
-                        help='Mosdepth output file containing coverage values.')
-    parser.add_argument('--cov_col', type=int, default=4,
-                        help='Column holding the coverage values.')
-    parser.add_argument('--output_prefix', type=str,
-                        help='Prefix to use for output files.')
-    parser.add_argument('--round', type=int, default=2, help='Rounding precision.')
-    parser.add_argument('--debug', action='store_true', help='Debug mode.')
+    parser = argparse.ArgumentParser(description="Process coverage values.")
+    parser.add_argument("mosdepth_regions", type=str,
+                        help="Mosdepth output file containing coverage values.")
+    parser.add_argument("--cov_col", type=int, default=4,
+                        help="Column holding the coverage values.")
+    parser.add_argument("--output_prefix", type=str,
+                        help="Prefix to use for output files.")
+    parser.add_argument("--round", type=int, default=2, help="Rounding precision.")
+    parser.add_argument("--debug", action="store_true", help="Debug mode.")
     return parser.parse_args()
 
 
@@ -36,15 +36,15 @@ def calculate_summary_statistics(
     mad_val: float = np.mean(np.abs(cov_data - mean_val))
 
     statistics = {
-        'cov_mean': round(mean_val, round_precision),
-        'cov_q1': round(cov_data.quantile(0.25), round_precision),
-        'cov_median': round(cov_data.median(), round_precision),
-        'cov_q3': round(cov_data.quantile(0.75), round_precision),
-        'cov_iqr': round(cov_data.quantile(0.75) - cov_data.quantile(0.25), round_precision),
-        'cov_stdev': round(cov_data.std(), round_precision),
-        'cov_mad': round(mad_val, round_precision),
-        'cov_percent_above_4x': percentage_greater_than_4x(df, cov_col, round_precision),
-        'cov_evenness_score': calculate_evenness_score(df, cov_col, round_precision)
+        "cov_mean": round(mean_val, round_precision),
+        "cov_q1": round(cov_data.quantile(0.25), round_precision),
+        "cov_median": round(cov_data.median(), round_precision),
+        "cov_q3": round(cov_data.quantile(0.75), round_precision),
+        "cov_iqr": round(cov_data.quantile(0.75) - cov_data.quantile(0.25), round_precision),
+        "cov_stdev": round(cov_data.std(), round_precision),
+        "cov_mad": round(mad_val, round_precision),
+        "cov_percent_above_4x": percentage_greater_than_4x(df, cov_col, round_precision),
+        "cov_evenness_score": calculate_evenness_score(df, cov_col, round_precision)
     }
 
     # Replace Nan values with null
@@ -107,7 +107,7 @@ def calculate_evenness_score(
         evenness_score = round(1 - (d2_count - (d2_sum / mean_coverage)) / coverage_count, round_precision)
     else:
         logging.warning("Mean coverage is zero. Evenness score will be set to null.")
-        evenness_score = 'null'
+        evenness_score = "null"
 
     logging.info("Evenness score: %s", evenness_score)
 
@@ -121,12 +121,12 @@ def open_file(file_path: str) -> pd.DataFrame:
     @return:
     """
 
-    if file_path.endswith('.gz'):
-        with gzip.open(file_path, 'rt') as f:
-            df = pd.read_csv(f, sep='\t', header=None)
+    if file_path.endswith(".gz"):
+        with gzip.open(file_path, "rt") as f:
+            df = pd.read_csv(f, sep="\t", header=None)
     else:
-        with open(file_path, 'rt') as f:
-            df = pd.read_csv(f, sep='\t', header=None)
+        with open(file_path, "rt") as f:
+            df = pd.read_csv(f, sep="\t", header=None)
 
     return df
 
@@ -141,7 +141,7 @@ def main():
     logging.info("Arguments: %s", args)
     logging.info("Calculating coverage statistics")
 
-    prefix = args.output_prefix if args.output_prefix else args.mosdepth_regions.split('.')[0]
+    prefix = args.output_prefix if args.output_prefix else args.mosdepth_regions.split(".")[0]
 
     df: pd.DataFrame = open_file(args.mosdepth_regions)
     logging.info("Opened file: %s", args.mosdepth_regions)
@@ -157,9 +157,9 @@ def main():
     summary_file = f"{prefix}.cov_stat_summary.json"
 
     logging.info("Writing summary statistics to file: %s", summary_file)
-    with open(summary_file, 'w') as f:
+    with open(summary_file, "w") as f:
         json.dump(statistics, f)
-        f.write('\n')
+        f.write("\n")
 
 
 if __name__ == "__main__":
