42fa14d9d935717b25f7f9e78aa46c81fbdf701f
Steve Huang <shuang@broadinstitute.org>
2026-02-18 23:54:51 -0500

    update base image from 22.04 to 24.04; install more dependencies; support requester-pays; better makefile

--- docker/lr-gcloud-samtools/re-auth.sh
+++ docker/lr-gcloud-samtools/re-auth.sh
@@ -2,5 +2,5 @@
 
 set -eu
 
-export GCS_OAUTH_TOKEN=`gcloud auth application-default print-access-token`
-
+export GCS_OAUTH_TOKEN=$(gcloud auth application-default print-access-token)
+export GCS_REQUESTER_PAYS_PROJECT=$(gcloud config get-value project)
